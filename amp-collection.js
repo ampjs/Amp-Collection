bf5ee75b59898c234b4ec9fa36d702ea2726281f
Paul Taylor <hello@ampersandwich.co.uk>
2016-06-21 17:19:27 +0100

    module update for PaginatedCollection class

--- amp-collection.js
+++ amp-collection.js
@@ -1,1 +1,6 @@
-module.exports = exports.default = require('./lib/AmpCollection.js');
+// module.exports = exports.default = require('./lib/AmpCollection.js');
+
+module.exports = {
+    AmpCollection: require('./lib/AmpCollection.js').default,
+    PaginatedCollection: require('./lib/PaginatedCollection.js').default
+};
