d186db63f13abd7032f928e1e2834cd078a6d97c
Paul Taylor <hello@ampersandwich.co.uk>
2016-06-21 17:18:34 +0100

    Paginated Collection class

---
+++ src/PaginatedCollection.js
@@ -0,0 +1,9 @@
+import AmpCollection from './AmpCollection.js';
+
+class PaginatedCollection extends AmpCollection {
+    constructor() {
+        super();
+    }
+}
+
+export default PaginatedCollection;
