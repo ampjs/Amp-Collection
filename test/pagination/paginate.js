f9149f3626c3268eae0e4242d1ccb92e431a2280
Paul Taylor <hello@ampersandwich.co.uk>
2016-06-22 20:51:33 +0100

    Pagination tests.

--- test/pagination/paginate.js
+++ test/pagination/paginate.js
@@ -39,6 +39,20 @@ describe('PaginatedCollection pages', function() {
             chai.expect(PaginatedCollection.all()).to.be.an('array');
             chai.expect(PaginatedCollection.all().length).to.equal(2);
         });
+
+        it('paginate() to have a default of 5.', function() {
+            PaginatedCollection.paginate(0);
+
+            chai.expect(PaginatedCollection.all()).to.be.an('array');
+            chai.expect(PaginatedCollection.all().length).to.equal(1);
+        });
+
+        it('paginate() accepts strings.', function() {
+            PaginatedCollection.paginate('2');
+
+            chai.expect(PaginatedCollection.all()).to.be.an('array');
+            chai.expect(PaginatedCollection.all().length).to.equal(2);
+        });
     });
 
     describe('page()', function() {
@@ -73,7 +87,6 @@ describe('PaginatedCollection pages', function() {
                 chai.expect(PaginatedCollection.pages.previous).to.equal(1);
             });
 
-
             it('{pages.next} isn\'t greater than allowed.', function() {
                 // Split into chunks of 1 for testing next.
                 PaginatedCollection.paginate(1);
@@ -81,8 +94,67 @@ describe('PaginatedCollection pages', function() {
                 chai.expect(PaginatedCollection.pages.next).to.be.a('number');
                 chai.expect(PaginatedCollection.pages.next).to.not.equal(5);
             });
+
+            it('{pages.next} isn\'t lower than allowed.', function() {
+                // Split into chunks of 1 for testing next.
+                PaginatedCollection.paginate(1).page(0);
+
+                chai.expect(PaginatedCollection.pages.current).to.be.a('number');
+                chai.expect(PaginatedCollection.pages.current).to.equal(1);
+                chai.expect(PaginatedCollection.pages.next).to.be.a('number');
+                chai.expect(PaginatedCollection.pages.next).to.equal(2);
+                chai.expect(PaginatedCollection.pages.next).to.not.equal(0);
+            });
         });
     });
 
-    describe('next()')
+    describe('next()', function() {
+        it('{pages.next} is greater.', function() {
+            var thePage = PaginatedCollection.paginate(1).page(2);
+
+            chai.expect(thePage.pages.next).to.be.a('number');
+            chai.expect(thePage.pages.next).to.equal(3);
+
+            thePage.next();
+
+            chai.expect(thePage.pages.next).to.be.a('number');
+            chai.expect(thePage.pages.next).to.equal(4);
+        });
+
+        it('{pages.next} is doesn\'t exceed {pages.last}.', function() {
+            var thePage = PaginatedCollection.paginate(1).page(4);
+
+            chai.expect(thePage.pages.next).to.be.a('number');
+            chai.expect(thePage.pages.next).to.equal(4);
+
+            thePage.next();
+
+            chai.expect(thePage.pages.next).to.not.equal(5);
+        });
+    });
+
+    describe('previous()', function() {
+        it('{pages.previous} is lower.', function() {
+            var thePage = PaginatedCollection.paginate(1).page(3);
+
+            chai.expect(thePage.pages.previous).to.be.a('number');
+            chai.expect(thePage.pages.previous).to.equal(2);
+
+            thePage.previous();
+
+            chai.expect(thePage.pages.previous).to.be.a('number');
+            chai.expect(thePage.pages.previous).to.equal(1);
+        });
+
+        it('{pages.previous} is doesn\'t go below 1.', function() {
+            var thePage = PaginatedCollection.paginate(1).page(2);
+
+            chai.expect(thePage.pages.previous).to.be.a('number');
+            chai.expect(thePage.pages.previous).to.equal(1);
+
+            thePage.previous();
+
+            chai.expect(thePage.pages.previous).to.not.equal(0);
+        });
+    });
 });
